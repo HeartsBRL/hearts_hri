a90d12c14fd309cc130f16b4d1fd8bc93f3d6627
hearts team brl <dd17883@my.bristol.ac.uk>
2018-11-21 14:53:05 +0000

    ain't broken

--- hearts_tts/scripts/tts.py
+++ hearts_tts/scripts/tts.py
@@ -6,16 +6,19 @@ import alsaaudio # added 26/02/2017 in Edinburgh
 import time
 
 mixer_spk = alsaaudio.Mixer(control='Master', id=0)
-mixer_mic = alsaaudio.Mixer(control='Capture', id=0) 
+mixer_mic = alsaaudio.Mixer(control='Capture', id=0)
 
+print("\n###### check alsaaudio is loaded #####\n")
+p = alsaaudio.__file__
+print("alsaaudio module path is:\n"+p+"\n")
 
 def callback(data):
     mixer_spk.setmute(0)
     mixer_mic.setrec(0)
-  
+
     lstr = len(data.data)
 
-    soundhandle.say(data.data)
+    #soundhandle.say(data.data)
     delay = lstr * 0.125
     time.sleep(delay)
 
